c88859558e60ced034a737c57c232bf8041fbb38
Raphael <clariasraph4@gmail.com>
2024-05-04 05:33:35 -0700

    C - Binary trees

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -7,6 +7,7 @@
  * @func: Pointer to a function to call for each node
  *
  * Description: This function traverses a binary tree using in-order traversal
+ * Return: Nothing
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
